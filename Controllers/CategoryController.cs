6f85eb716c5cf610256bc056809c49f45c6d5968
hailong2002 <103204476+hailong2002@users.noreply.github.com>
2023-02-25 10:17:44 +0700

    yare yare

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -18,13 +18,16 @@ namespace ProjectCMS.Controllers
             _dbContext = dbContext;
         }
 
+        // Get all categories
         [HttpGet]
-        public async Task<IActionResult> GetCategory() 
+        public async Task<IActionResult> GetAllCategories() 
         {
             List<Category> categories = await _dbContext._categories.ToListAsync();
             return Ok(categories);
         }
 
+
+        // Create a category
         [HttpPost]
         public async Task<IActionResult> CreateCategory(CategoryViewModel category)
         {
@@ -33,29 +36,66 @@ namespace ProjectCMS.Controllers
                 Category newCate = new Category();
                 newCate.Name = category.Name;
                 newCate.Content = category.Content;
+                newCate.AddedDate = category.AddedDate;
 
-                _dbContext._categories.Add(newCate);
-                _dbContext.SaveChanges();
+                await _dbContext._categories.AddAsync(newCate);
+                await  _dbContext.SaveChangesAsync();
                 return Ok(await _dbContext._categories.ToListAsync());
             }
             return BadRequest();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCategory(int id)
+
+        // Get a category by id
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetCategory([FromRoute] int id)
+        {
+            return Ok(await _dbContext._categories.FindAsync(id));
+        }
+
+
+        // Delete category
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteCategory([FromRoute]  int id)
         {
             var category = await _dbContext._categories.FindAsync(id);
             if(category.Ideas == null)
             {
                 _dbContext._categories.Remove(category);
-                _dbContext.SaveChanges();
+                await _dbContext.SaveChangesAsync();
                 return Ok("Successfully deleted ~.~ ");
             }
             if(category.Ideas != null)
             {
-                return BadRequest("Cannot delete ! This category has events.");
+                return BadRequest("Cannot delete ! This category has ideas.");
             }
-            return NotFound("Can't find category");
+            return NotFound("Can't find category !");
         }
+
+
+        // Edit category
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> EditCategory(CategoryViewModel newCate, [FromRoute] int id)
+        {
+            var category = await _dbContext._categories.FindAsync(id);
+            if (category != null)
+            {
+                category.Content = newCate.Content;
+                category.AddedDate = newCate.AddedDate;
+                category.Name = newCate.Name;
+                await _dbContext.SaveChangesAsync();
+                return Ok("Successfully Edited. " + category.Name);
+            }
+            return BadRequest("Can't find category !");
+        }
+
+
+
+
+
+
     }
 }
