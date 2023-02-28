4b0068c1affb5e04e631d60ef3ecc0125f195be1
Đỗ Nguyễn Huy Hoàng <103497974+keoConCho@users.noreply.github.com>
2023-02-28 12:53:06 +0700

    Register

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ProjectCMS.Data;
 using ProjectCMS.Models;
+using ProjectCMS.ViewModels;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace ProjectCMS.Controllers
@@ -12,10 +16,11 @@ namespace ProjectCMS.Controllers
     {
         private readonly ApplicationDbContext _dbContext;
         public User user = new();
-
-        public AuthController(ApplicationDbContext dbContext)
+        private readonly IConfiguration _configuration;
+        public AuthController(ApplicationDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _configuration = configuration;
         }
         [HttpGet]
         public async Task<IActionResult> getAllUser()
@@ -27,12 +32,13 @@ namespace ProjectCMS.Controllers
         [HttpPost("Register")]
         public async Task<IActionResult> Register(UserDTO usr)
         {
-            CreatePasswordHash(usr.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            CreatePasswordHash(usr.password, out byte[] passwordHash, out byte[] passwordSalt);
             
-            user.UserName= usr.UserName;
+            user.UserName= usr.userName;
             user.Email = usr.Email;
+            user.Role = usr.Role;
             user.PasswordHash = passwordHash;
-            user.PasswordSalt = passwordSalt;
+            user.PasswordSalt = passwordSalt;       
             _dbContext._users.Add(user);
             _dbContext.SaveChanges();
             return Ok(user);
@@ -41,19 +47,36 @@ namespace ProjectCMS.Controllers
         public async Task<IActionResult> Login(UserDTO rq)
         {
             List<User> users = await _dbContext._users.ToListAsync();
-            foreach (User user in users) 
+            
+            foreach (var user in users) 
             {
-                if(user.UserName != rq.UserName) 
-                {
-                    return BadRequest("Wrong username ");
-                    
-                }
-                if (!Verify(rq.Password, user.PasswordHash, user.PasswordSalt))
-                {
-                    return BadRequest("wrong password");
+                if(user.UserName == rq.userName) 
+                {                   
+                    if (Verify(rq.password, user.PasswordHash, user.PasswordSalt))
+                    {
+                        string token = tokenMethod(user);
+                        return Ok(token);
+                    }                   
                 }
             }
-            return Ok("Login success");
+            return BadRequest("wrong username or password");
+        }
+        private string tokenMethod(User user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+            };
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var cred = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken
+                (
+                    claims:claims,
+                    expires:DateTime.Now.AddDays(1),
+                    signingCredentials:cred
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
         private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt) 
         {
---
+++ ViewModels/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectCMS.ViewModels
+{
+    public class UserDTO
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string? Email { get; set; }
+        public int? Role { get;  set; }
+    }
+}
